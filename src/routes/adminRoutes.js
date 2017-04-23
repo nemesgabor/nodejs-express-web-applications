8bbf0619a29cf7323d4409b9d67532c81ba166b7
nemesgabor <nemesgabor@outlook.com>
2017-04-23 18:28:03 +0200

    Before Login

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var adminRouter = express.Router();
-
+var mongodb = require('mongodb').MongoClient;
+var books = [
+        {
+            Title:'Valami1',
+            Author:'Valaki1',
+            Genre: 'Biography',
+            Read: false
+        },
+        {
+            Title:'Valami2',
+            Author:'Valaki2',
+            Genre: 'Biography',
+            Read: false
+        },
+        {
+            Title:'Valami3',
+            Author:'Valaki3',
+            Genre: 'Biography',
+            Read: false
+        }
+    ];
 var router = function(nav) {
     adminRouter.route('/addBooks')
         .get(function(req,res) {
-            res.send('Instering books');
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err,db) {
+                var collection = db.collection('books');
+                collection.insertMany(books, function(err,results) {
+                    res.send(results);
+                    db.close();
+                });
+            });
+            //res.send('Instering books');
         });
     return adminRouter;
 };
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,42 +1,37 @@
 var express = require('express');
 var bookRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 
 var router = function(nav) {
-    var books = [
-        {
-            Title:'Valami1',
-            Author:'Valaki1',
-            Genre: 'Biography',
-            Read: false
-        },
-        {
-            Title:'Valami2',
-            Author:'Valaki2',
-            Genre: 'Biography',
-            Read: false
-        },
-        {
-            Title:'Valami3',
-            Author:'Valaki3',
-            Genre: 'Biography',
-            Read: false
-        }
-    ];
+
     bookRouter.route('/')
         .get(function(req,res) {
-            res.render('bookListView',{
-                    title:'Books',
-                    nav: nav,
-                    books:books
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err,db) {
+                var collection = db.collection('books');
+                collection.find({}).toArray(function(err,results) {
+                    res.render('bookListView',{
+                        title:'Books',
+                        nav: nav,
+                        books:results
+                    });
                 });
+            });
         });
     bookRouter.route('/:id')
         .get(function(req,res) {
-            var Id = req.params.id;
-            res.render('bookView',{
-                title:'Books',
-                nav: nav,
-                book:books[Id]
+            var id = new ObjectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err,db) {
+                var collection = db.collection('books');
+                collection.findOne({_id: id},function(err,results) {
+                    res.render('bookView',{
+                        title:'Books',
+                        nav: nav,
+                        book:results
+                    });
+                });
             });
         });
     return bookRouter;
