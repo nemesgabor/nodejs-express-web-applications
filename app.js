4c1222e3ef69065e0e698ef281039d1636623905
nemesgabor <nemesgabor@outlook.com>
2017-04-23 21:58:09 +0200

    Auth  done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 var app = express();
 
 var port = process.env.PORT || 5000;
@@ -14,6 +17,10 @@ var authRouter = require('./src/routes/authRoutes')(nav);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport')(app);
 
 app.set('views','./src/views');
 
